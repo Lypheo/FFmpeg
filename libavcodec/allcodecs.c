059eeb582408b4259fab156fefe1f9ec316e291b
Lypheo <saifullah124@hotmail.de>
2023-03-26 21:10:04 +0200

    Add SVT-VP9 encoder

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -794,6 +794,7 @@ extern const FFCodec ff_libvpx_vp8_encoder;
 extern const FFCodec ff_libvpx_vp8_decoder;
 extern FFCodec ff_libvpx_vp9_encoder;
 extern FFCodec ff_libvpx_vp9_decoder;
+extern FFCodec ff_libsvt_vp9_encoder;
 /* preferred over libwebp */
 extern const FFCodec ff_libwebp_anim_encoder;
 extern const FFCodec ff_libwebp_encoder;
